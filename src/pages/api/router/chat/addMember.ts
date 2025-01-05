70b3ea8606aca3d5cf07142f3e776c8f9bc5a4b9
Ding Ruoqian <bubuding0809@gmail.com>
2025-01-05 23:33:26 +0800

    feat: add API route to handle adding members to chat

---
+++ src/pages/api/router/chat/addMember.ts
@@ -0,0 +1,44 @@
+import { createCaller } from '#/server/api/root';
+import { db } from '#/server/db';
+import { bot } from '#/server/tgbot';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'PATCH') {
+    const { chat_id: chatId, user_id: userId } = JSON.parse(req.body);
+
+    const trpc = createCaller({
+      db,
+      bot,
+    });
+
+    try {
+      const existingChat = await trpc.chat.getChat({
+        chatId,
+      });
+
+      if (!existingChat) {
+        res.status(404).json({ message: `Chat not found: ${chatId}` });
+        return;
+      }
+
+      const updatedChat = await trpc.chat.addMember({
+        chatId,
+        userId,
+      });
+
+      res
+        .status(200)
+        .json({ message: `Added member: ${userId} to chat: ${chatId}`, chat: updatedChat });
+      return;
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Failed to add member to chat' });
+      return;
+    }
+  }
+
+  // Handle other HTTP methods
+  res.setHeader('Allow', ['PATCH']);
+  res.status(405).end(`Method ${req.method} Not Allowed`);
+}
